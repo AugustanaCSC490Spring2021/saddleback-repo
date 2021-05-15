86d16c31b68afa7c588162a49c55a16bd3bac569
kirubelM <kirubelmognehode18@augustana.edu>
2021-05-14 21:33:52 -0500

    Added Location markers after Search feature

--- src/map/Map.js
+++ src/map/Map.js
@@ -4,6 +4,7 @@ import { mapStyles } from "./MapStyles";
 import Markers from "../component/Markers";
 import AcesRoute from "../component/AcesRoute";
 import SearchBox from "../component/searchBox";
+import {markersLocation} from './MarkersLocation';
 
 const containerStyle = {
   width: "100vw",
@@ -31,42 +32,16 @@ const options = {
     strictBounds: false,
   },
 };
-const markers = [
- 
-  {
-    name: "Hanson Expansion",
-    lat: "41.504297",
-    long: "-90.551095",
-    type: "academic",
-  },
-  {
-    name: "Old Main",
-    lat: "41.504439",
-    long: "-90.549609",
-    type: "academic",
-  },
-  {
-    name: "Lot B",
-    lat: "41.505329",
-    long: "-90.548099",
-    type: "parking",
-  },
-  {
-    name: "Olin",
-    lat: "41.50315",
-    long: "-90.550581",
-    type: "academic",
-  },
-];
+
 export default class Map extends Component {
 
   state = {
-    filteredData: markers,
+    filteredData: markersLocation,
   };
 
   handleSearch = (e) => {
     const { value } = e.target;
-    let filteredData = markers.filter((item) => {
+    let filteredData = markersLocation.filter((item) => {
       let currentvalue = item.name;
       return currentvalue.toLowerCase().includes(value.toLowerCase());
     });
